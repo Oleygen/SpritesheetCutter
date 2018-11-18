b932933760d0989f74de4afc2385f5b70cedf8fc
oleygen ua <oleygenua@oleygens-iMac.local>
2018-11-18 18:20:11 +0200

    Initial commit

---
+++ SpritesheetCutter/SpritesheetCutter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SpritesheetCutter
+//
+//  Created by oleygen ua on 11/18/18.
+//  Copyright © 2018 oleygen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SpritesheetCutter/SpritesheetCutter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpritesheetCutter
+//
+//  Created by oleygen ua on 11/18/18.
+//  Copyright © 2018 oleygen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
